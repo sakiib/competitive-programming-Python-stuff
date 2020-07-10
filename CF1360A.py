d450555290cc62178d124b8fe1a070a0f02eed4d
Sakib Alamin <sakibalamin162@gmail.com>
2020-07-10 16:12:09 +0600

    Added cf1360A

---
+++ CF1360A.py
@@ -0,0 +1,9 @@
+def main():
+    for tc in range(int(input())):
+        a, b = map(int, input().split())
+        x = min(max(2 * a, b), max(a, 2 * b))
+        print(x * x)
+
+
+if __name__ == '__main__':
+    main()
